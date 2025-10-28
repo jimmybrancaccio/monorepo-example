2baf2913f50427ef9a23ada65744e64927764e9b
Jimmy B. <jimmybrancaccio@icloud.com>
2025-10-28 12:14:44 -0500

    Change envDir path in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   publicDir: "../public",
   root: "./src",
-  envDir: "../",
+  envDir: "./",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
